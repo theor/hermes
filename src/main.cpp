baea56d5e0040174da5fdd8f6535ada06ef0a951
Theo Richart <theor@unity3d.com>
2022-07-24 13:55:39 -0400

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,382 +1,219 @@
-/*********
-  Complete project details at https://randomnerdtutorials.com
+/*
+  Rui Santos
+  Complete project details
+   - Arduino IDE: https://RandomNerdTutorials.com/esp32-ota-over-the-air-arduino/
+   - VS Code: https://RandomNerdTutorials.com/esp32-ota-over-the-air-vs-code/
+  
+  This sketch shows a Basic example from the AsyncElegantOTA library: ESP32_Async_Demo
+  https://github.com/ayushsharma82/AsyncElegantOTA
+*/
 
-  This is an example for our Monochrome OLEDs based on SSD1306 drivers. Pick one up today in the adafruit shop! ------> http://www.adafruit.com/category/63_98
-  This example is for a 128x32 pixel display using I2C to communicate 3 pins are required to interface (two I2C and one reset).
-  Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
-  Written by Limor Fried/Ladyada for Adafruit Industries, with contributions from the open source community. BSD license, check license.txt for more information All text above, and the splash screen below must be included in any redistribution.
-*********/
-
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include "WifiConfig.h"
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <WiFi.h>
-#include <WifiConfig.h>
-#include <HTTPClient.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//variabls for blinking an LED with Millis
+const int led = 2; // ESP32 Pin to which onboard LED is connected
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const long interval = 300;  // interval at which to blink (milliseconds)
+int ledState = LOW;  // ledState used to set the LED
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+AsyncWebServer server(80);
 
-#define NUMFLAKES 10 // Number of snowflakes in the animation example
-
-#define LOGO_HEIGHT 16
-#define LOGO_WIDTH 16
-static const unsigned char PROGMEM logo_bmp[] =
-    {B00000000, B11000000,
-     B00000001, B11000000,
-     B00000001, B11000000,
-     B00000011, B11100000,
-     B11110011, B11100000,
-     B11111110, B11111000,
-     B01111110, B11111111,
-     B00110011, B10011111,
-     B00011111, B11111100,
-     B00001101, B01110000,
-     B00011011, B10100000,
-     B00111111, B11100000,
-     B00111111, B11110000,
-     B01111100, B11110000,
-     B01110000, B01110000,
-     B00000000, B00110000};
-     
-#define Threshold 40 /* Greater the value, more the sensitivity */
-
-
-void initWiFi()
-{
-  Serial.println("Connecting to WiFi ..");
+void setup(void) {
+pinMode(led, OUTPUT);
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
-  uint8_t i = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    if (i++ % 2 == 0)
-      Serial.print('.');
-    else
-      Serial.print(" .");
-    // Serial.Serial();
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
-  // Serial.clearSerial();
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(SSID);
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  display.clearDisplay();
 
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
-  display.println(WiFi.localIP().toString());
-  display.display();
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
+
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
-void pullMessage() {
-  HTTPClient http;
-  http.begin("https://theor.github.io/hermes/content.txt");
-  int httpResponseCode = http.GET();
-    display.clearDisplay();
-    display.setCursor(0,0);
-  if (httpResponseCode > 0)
-  {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    String payload = http.getString();
-    
-    display.println(payload);
-    display.display();
+void loop(void) {
+//loop to blink without delay
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+  // save the last time you blinked the LED
+  previousMillis = currentMillis;
+  // if the LED is off turn it on and vice-versa:
+  ledState = not(ledState);
+  // set the LED with the ledState of the variable:
+  digitalWrite(led,  ledState);
   }
-  else
-  {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-
-}
-void callback(){
-  //placeholder callback function
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  int i = 0;
-  while (!Serial && i++ < 1000)
-    ;
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ; // Don't proceed, loop forever
-  }
+// /*********
+//   Complete project details at https://randomnerdtutorials.com
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
+//   This is an example for our Monochrome OLEDs based on SSD1306 drivers. Pick one up today in the adafruit shop! ------> http://www.adafruit.com/category/63_98
+//   This example is for a 128x32 pixel display using I2C to communicate 3 pins are required to interface (two I2C and one reset).
+//   Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+//   Written by Limor Fried/Ladyada for Adafruit Industries, with contributions from the open source community. BSD license, check license.txt for more information All text above, and the splash screen below must be included in any redistribution.
+// *********/
 
-  // Clear the buffer
-  display.setRotation(1);
-  display.clearDisplay();
+// #include <WiFi.h>
+// #include <WifiConfig.h>
+// #include <HTTPClient.h>
 
-  // Draw a single pixel in white
-  display.drawPixel(10, 10, WHITE);
+// #define SCREEN_WIDTH 128 // OLED display width, in pixels
+// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  display.display();
-  initWiFi();
-  pullMessage();
+// // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-  delay(10000);
+// #define NUMFLAKES 10 // Number of snowflakes in the animation example
 
-  touchAttachInterrupt(T3, callback, Threshold);
+// #define LOGO_HEIGHT 16
+// #define LOGO_WIDTH 16
+// static const unsigned char PROGMEM logo_bmp[] =
+//     {B00000000, B11000000,
+//      B00000001, B11000000,
+//      B00000001, B11000000,
+//      B00000011, B11100000,
+//      B11110011, B11100000,
+//      B11111110, B11111000,
+//      B01111110, B11111111,
+//      B00110011, B10011111,
+//      B00011111, B11111100,
+//      B00001101, B01110000,
+//      B00011011, B10100000,
+//      B00111111, B11100000,
+//      B00111111, B11110000,
+//      B01111100, B11110000,
+//      B01110000, B01110000,
+//      B00000000, B00110000};
+     
+// #define Threshold 40 /* Greater the value, more the sensitivity */
 
-  //Configure Touchpad as wakeup source
-  esp_sleep_enable_touchpad_wakeup();
 
-  //Go to sleep now
-  Serial.println("Going to sleep now");
-  display.clearDisplay();
-  display.ssd1306_command(SSD1306_DISPLAYOFF);
-  esp_deep_sleep_start();
-}
-
-void loop()
-{
-  if(!digitalRead(0))
-    pullMessage();
-}
-
-// void testdrawrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2; i+=2) {
-//     display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
-//     display.display(); // Update screen with each newly-drawn rectangle
-//     delay(1);
+// void initWiFi()
+// {
+//   Serial.println("Connecting to WiFi ..");
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(SSID, PASSWORD);
+//   uint8_t i = 0;
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     if (i++ % 2 == 0)
+//       Serial.print('.');
+//     else
+//       Serial.print(" .");
+//     // Serial.Serial();
+//     delay(500);
 //   }
-
-//   delay(2000);
-// }
-
-// void testfillrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2; i+=3) {
-//     // The INVERSE color is used so rectangles alternate white/black
-//     display.fillRect(i, i, display.width()-i*2, display.height()-i*2, INVERSE);
-//     display.display(); // Update screen with each newly-drawn rectangle
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testdrawcircle(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-//     display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testfillcircle(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=max(display.width(),display.height())/2; i>0; i-=3) {
-//     // The INVERSE color is used so circles alternate white/black
-//     display.fillCircle(display.width() / 2, display.height() / 2, i, INVERSE);
-//     display.display(); // Update screen with each newly-drawn circle
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testdrawroundrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2-2; i+=2) {
-//     display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i,
-//       display.height()/4, WHITE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testfillroundrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2-2; i+=2) {
-//     // The INVERSE color is used so round-rects alternate white/black
-//     display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i,
-//       display.height()/4, INVERSE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testdrawtriangle(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<max(display.width(),display.height())/2; i+=5) {
-//     display.drawTriangle(
-//       display.width()/2  , display.height()/2-i,
-//       display.width()/2-i, display.height()/2+i,
-//       display.width()/2+i, display.height()/2+i, WHITE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testfilltriangle(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=max(display.width(),display.height())/2; i>0; i-=5) {
-//     // The INVERSE color is used so triangles alternate white/black
-//     display.fillTriangle(
-//       display.width()/2  , display.height()/2-i,
-//       display.width()/2-i, display.height()/2+i,
-//       display.width()/2+i, display.height()/2+i, INVERSE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testdrawchar(void) {
+//   // Serial.clearSerial();
+//   Serial.println(WiFi.localIP());
 //   display.clearDisplay();
 
 //   display.setTextSize(1);      // Normal 1:1 pixel scale
 //   display.setTextColor(WHITE); // Draw white text
 //   display.setCursor(0, 0);     // Start at top-left corner
-//   display.cp437(true);         // Use full 256 char 'Code Page 437' font
+//   display.println(WiFi.localIP().toString());
+//   display.display();
+// }
 
-//   // Not all the characters will fit on the display. This is normal.
-//   // Library will draw what it can and the rest will be clipped.
-//   for(int16_t i=0; i<256; i++) {
-//     if(i == '\n') display.write(' ');
-//     else          display.write(i);
+// void pullMessage() {
+//   HTTPClient http;
+//   http.begin("https://theor.github.io/hermes/content.txt");
+//   int httpResponseCode = http.GET();
+//     display.clearDisplay();
+//     display.setCursor(0,0);
+//   if (httpResponseCode > 0)
+//   {
+//     Serial.print("HTTP Response code: ");
+//     Serial.println(httpResponseCode);
+//     String payload = http.getString();
+    
+//     display.println(payload);
+//     display.display();
+//   }
+//   else
+//   {
+//     Serial.print("Error code: ");
+//     Serial.println(httpResponseCode);
+//   }
+//   // Free resources
+//   http.end();
+
+// }
+// void callback(){
+//   //placeholder callback function
+// }
+
+// void setup()
+// {
+//   Serial.begin(115200);
+//   int i = 0;
+//   while (!Serial && i++ < 1000)
+//     ;
+
+//   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+//   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+//   {
+//     Serial.println(F("SSD1306 allocation failed"));
+//     for (;;)
+//       ; // Don't proceed, loop forever
 //   }
 
-//   display.display();
-//   delay(2000);
-// }
+//   // Show initial display buffer contents on the screen --
+//   // the library initializes this with an Adafruit splash screen.
 
-// void testdrawstyles(void) {
+//   // Clear the buffer
+//   display.setRotation(1);
 //   display.clearDisplay();
 
-//   display.setTextSize(1);             // Normal 1:1 pixel scale
-//   display.setTextColor(WHITE);        // Draw white text
-//   display.setCursor(0,0);             // Start at top-left corner
-//   display.println(F("Hello, world!"));
+//   // Draw a single pixel in white
+//   display.drawPixel(10, 10, WHITE);
 
-//   display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
-//   display.println(3.141592);
-
-//   display.setTextSize(2);             // Draw 2X-scale text
-//   display.setTextColor(WHITE);
-//   display.print(F("0x")); display.println(0xDEADBEEF, HEX);
-
+//   // Show the display buffer on the screen. You MUST call display() after
+//   // drawing commands to make them visible on screen!
 //   display.display();
-//   delay(2000);
-// }
+//   initWiFi();
+//   pullMessage();
 
-// void testscrolltext(void) {
+//   delay(10000);
+
+//   touchAttachInterrupt(T3, callback, Threshold);
+
+//   //Configure Touchpad as wakeup source
+//   esp_sleep_enable_touchpad_wakeup();
+
+//   //Go to sleep now
+//   Serial.println("Going to sleep now");
 //   display.clearDisplay();
-
-//   display.setTextSize(2); // Draw 2X-scale text
-//   display.setTextColor(WHITE);
-//   display.setCursor(10, 0);
-//   display.println(F("scroll"));
-//   display.display();      // Show initial text
-//   delay(100);
-
-//   // Scroll in various directions, pausing in-between:
-//   display.startscrollright(0x00, 0x0F);
-//   delay(2000);
-//   display.stopscroll();
-//   delay(1000);
-//   display.startscrollleft(0x00, 0x0F);
-//   delay(2000);
-//   display.stopscroll();
-//   delay(1000);
-//   display.startscrolldiagright(0x00, 0x07);
-//   delay(2000);
-//   display.startscrolldiagleft(0x00, 0x07);
-//   delay(2000);
-//   display.stopscroll();
-//   delay(1000);
+//   display.ssd1306_command(SSD1306_DISPLAYOFF);
+//   esp_deep_sleep_start();
 // }
 
-// void testdrawbitmap(void) {
-//   display.clearDisplay();
-
-//   display.drawBitmap(
-//     (display.width()  - LOGO_WIDTH ) / 2,
-//     (display.height() - LOGO_HEIGHT) / 2,
-//     logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
-//   display.display();
-//   delay(1000);
-// }
-
-// #define XPOS   0 // Indexes into the 'icons' array in function below
-// #define YPOS   1
-// #define DELTAY 2
-
-// void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-//   int8_t f, icons[NUMFLAKES][3];
-
-//   // Initialize 'snowflake' positions
-//   for(f=0; f< NUMFLAKES; f++) {
-//     icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-//     icons[f][YPOS]   = -LOGO_HEIGHT;
-//     icons[f][DELTAY] = random(1, 6);
-//     Serial.print(F("x: "));
-//     Serial.print(icons[f][XPOS], DEC);
-//     Serial.print(F(" y: "));
-//     Serial.print(icons[f][YPOS], DEC);
-//     Serial.print(F(" dy: "));
-//     Serial.println(icons[f][DELTAY], DEC);
-//   }
-
-//   for(;;) { // Loop forever...
-//     display.clearDisplay(); // Clear the display buffer
-
-//     // Draw each snowflake:
-//     for(f=0; f< NUMFLAKES; f++) {
-//       display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
-//     }
-
-//     display.display(); // Show the display buffer on the screen
-//     delay(200);        // Pause for 1/10 second
-
-//     // Then update coordinates of each flake...
-//     for(f=0; f< NUMFLAKES; f++) {
-//       icons[f][YPOS] += icons[f][DELTAY];
-//       // If snowflake is off the bottom of the screen...
-//       if (icons[f][YPOS] >= display.height()) {
-//         // Reinitialize to a random position, just off the top
-//         icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-//         icons[f][YPOS]   = -LOGO_HEIGHT;
-//         icons[f][DELTAY] = random(1, 6);
-//       }
-//     }
-//   }
+// void loop()
+// {
+//   if(!digitalRead(0))
+//     pullMessage();
 // }
