13803af6f8632a6d65d8caacc56ed5297f556d60
Theo Richart <theor@unity3d.com>
2022-07-24 11:42:50 -0400

    v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,126 @@
 /*********
   Complete project details at https://randomnerdtutorials.com
-  
+
   This is an example for our Monochrome OLEDs based on SSD1306 drivers. Pick one up today in the adafruit shop! ------> http://www.adafruit.com/category/63_98
   This example is for a 128x32 pixel display using I2C to communicate 3 pins are required to interface (two I2C and one reset).
   Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
-  Written by Limor Fried/Ladyada for Adafruit Industries, with contributions from the open source community. BSD license, check license.txt for more information All text above, and the splash screen below must be included in any redistribution. 
+  Written by Limor Fried/Ladyada for Adafruit Industries, with contributions from the open source community. BSD license, check license.txt for more information All text above, and the splash screen below must be included in any redistribution.
 *********/
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+#include <WifiConfig.h>
+#include <HTTPClient.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
+#define NUMFLAKES 10 // Number of snowflakes in the animation example
 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
+#define LOGO_HEIGHT 16
+#define LOGO_WIDTH 16
 static const unsigned char PROGMEM logo_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+    {B00000000, B11000000,
+     B00000001, B11000000,
+     B00000001, B11000000,
+     B00000011, B11100000,
+     B11110011, B11100000,
+     B11111110, B11111000,
+     B01111110, B11111111,
+     B00110011, B10011111,
+     B00011111, B11111100,
+     B00001101, B01110000,
+     B00011011, B10100000,
+     B00111111, B11100000,
+     B00111111, B11110000,
+     B01111100, B11110000,
+     B01110000, B01110000,
+     B00000000, B00110000};
+     
+#define Threshold 40 /* Greater the value, more the sensitivity */
 
-  
-void testdrawline() {
-  int16_t i;
 
-  display.clearDisplay(); // Clear display buffer
-
-  for(i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
-    display.display(); // Update screen with each newly-drawn line
-    delay(1);
+void initWiFi()
+{
+  Serial.println("Connecting to WiFi ..");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    if (i++ % 2 == 0)
+      Serial.print('.');
+    else
+      Serial.print(" .");
+    // Serial.Serial();
+    delay(500);
   }
-  for(i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
+  // Serial.clearSerial();
+  Serial.println(WiFi.localIP());
   display.clearDisplay();
 
-  for(i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for(i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for(i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for(i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for(i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  for(i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000); // Pause for 2 seconds
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.println(WiFi.localIP().toString());
+  display.display();
 }
 
+void pullMessage() {
+  HTTPClient http;
+  http.begin("https://theor.github.io/hermes/content.txt");
+  int httpResponseCode = http.GET();
+    display.clearDisplay();
+    display.setCursor(0,0);
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    
+    display.println(payload);
+    display.display();
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
 
-void setup() {
+}
+void callback(){
+  //placeholder callback function
+}
+
+void setup()
+{
   Serial.begin(115200);
+  int i = 0;
+  while (!Serial && i++ < 1000)
+    ;
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
+  display.setRotation(1);
   display.clearDisplay();
 
   // Draw a single pixel in white
@@ -127,48 +129,27 @@ void setup() {
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
   display.display();
-  delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
+  initWiFi();
+  pullMessage();
 
-  testdrawline();      // Draw many lines
+  delay(10000);
 
-  // testdrawrect();      // Draw rectangles (outlines)
+  touchAttachInterrupt(T3, callback, Threshold);
 
-  // testfillrect();      // Draw rectangles (filled)
+  //Configure Touchpad as wakeup source
+  esp_sleep_enable_touchpad_wakeup();
 
-  // testdrawcircle();    // Draw circles (outlines)
-
-  // testfillcircle();    // Draw circles (filled)
-
-  // testdrawroundrect(); // Draw rounded rectangles (outlines)
-
-  // testfillroundrect(); // Draw rounded rectangles (filled)
-
-  // testdrawtriangle();  // Draw triangles (outlines)
-
-  // testfilltriangle();  // Draw triangles (filled)
-
-  // testdrawchar();      // Draw characters of the default font
-
-  // testdrawstyles();    // Draw 'stylized' characters
-
-  // testscrolltext();    // Draw scrolling text
-
-  // testdrawbitmap();    // Draw a small bitmap image
-
-  // // Invert and restore display, pausing in-between
-  // display.invertDisplay(true);
-  // delay(1000);
-  // display.invertDisplay(false);
-  // delay(1000);
-
-  // testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
+  //Go to sleep now
+  Serial.println("Going to sleep now");
+  display.clearDisplay();
+  display.ssd1306_command(SSD1306_DISPLAYOFF);
+  esp_deep_sleep_start();
 }
 
-void loop() {
+void loop()
+{
+  if(!digitalRead(0))
+    pullMessage();
 }
 
 // void testdrawrect(void) {
